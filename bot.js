f4448f9f5c403b9d45307b52b9cdc010d8d3901c
R-Men <huser.gaetan@hotmail.com>
2018-03-27 00:02:59 +0200

    starting bot

--- bot.js
+++ bot.js
@@ -1,69 +1,21 @@
-const Telegraf = require('telegraf');   // Module to use Telegraf API.
-const {Extra, Markup} = Telegraf;   // Extract Extra, Markups from Telegraf module.
-const config = require('./config'); // Configuration file that holds telegraf_token API key.
+const Telegraf = require('telegraf');
+const config = require('./config');
 
-const bot = new Telegraf(config.telegraf_token);    // Let's instantiate a bot using our token.
+const bot = new Telegraf(config.API_KEY);
 
-// We can get bot nickname from bot informations. This is particularly useful for groups.
+const c = require('./library/commands');
+
+
+
 bot.telegram.getMe().then((bot_informations) => {
     bot.options.username = bot_informations.username;
 console.log("Server has initialized bot nickname. Nick: "+bot_informations.username);
 });
 
-// Command example, pretty easy. Each callback passes as parameter the context.
-// Context data includes message info, timestamp, etc; check the official documentation or print ctx.
-bot.command('start', (ctx) => ctx.reply('Bot started.'));
+
+bot.start((ctx) => ctx.reply('Give me your class.'));
+
+bot.command('list', (ctx) => c.commands.list(ctx));
 
 
-/*
-// Hears, instead of command, check if the given word or regexp is CONTAINED in user input and not necessarly at beginning.
-bot.hears('ymca', (ctx) => ctx.reply("*sing* It's fun to stay at the Y.M.C.A.!"));
-bot.hears(/torino/i, (ctx) => ctx.reply("Someone said Torino!?"));
-
-if(query.startsWith("/")){  // If user input is @yourbot /command
-    if(query.startsWith("/add")){ // If user input is @yourbot /add
-        // In this case we want to add a new deadline
-        // It will be shown as a tooltip. You can add more than 1 element in this JSON array. Check API usage "InlineResultVoice".
-        return ctx.answerInlineQuery([
-            {
-                type: 'voice',  // It's a voice file.
-                id: ctx.update.inline_query.id,    // We reflect the same ID of the request back.
-                title: 'Send audio file sample.ogg',    // Message appearing in tooltip.
-                voice_url: 'https://upload.wikimedia.org/wikipedia/commons/c/c8/Example.ogg',
-                voice_duration: 16, // We can specify optionally the length in seconds.
-                caption: '[BOT] Audio file sample.ogg!' // What appears after you send voice file.
-            }
-        ]);
-    }
-}else{  // If user input is @yourbot name
-    let name_target = query;    // Let's assume the query is actually the name.
-    let message_length = name_target.length;    // Name length. We want to ensure it's > 0.
-    if(message_length > 0){
-        let full_message;
-        let dice=Math.floor(Math.random()*8)+1; // Let's throw a dice for a random message. (1, 8)
-        switch(dice){
-            case 1: full_message = "IMHO, "+name_target+" sucks."; break;
-            case 2: full_message = "IMHO, "+name_target+" is awesome"; break;
-            case 3: full_message = name_target+" is not a nice people for me..."; break;
-            case 4: full_message = name_target+" for me you are c- Eh! You wanted!"; break;
-            case 5: full_message = "Whoa! "+name_target+" is very cool!"; break;
-            case 6: full_message = "Grifondoro! No wait, "+name_target+" you're such a noob."; break;
-            case 7: full_message = "Sometimes I ask myself why people like "+name_target+" dress up and walk around like that..."; break;
-            case 8: full_message = "Watch him! Watch! "+name_target+" is so ugly!"; break;
-        }
-        // Let's return a single tooltip, not cached (In order to always change random value).
-        return ctx.answerInlineQuery([{
-            type: 'article',
-            id: ctx.update.inline_query.id,
-            title: 'You have inserted: '+name_target,
-            description: 'What does '+bot.options.username+' thinks about '+name_target+'?',
-            input_message_content: {message_text: full_message}
-        }], {cache_time: 0});
-    }
-}
-})
-*/
-
-
-// Start bot polling in order to not terminate Node.js application.
-bot.startPolling()
+bot.startPolling();
--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
 
 // R-Men TestBot
-module.exports = {telegraf_token: '304562828:AAG7Vd1MaoMxvX8dMgZlNpMpw_-9cZfiYy8'};
+module.exports = {API_KEY: '304562828:AAG7Vd1MaoMxvX8dMgZlNpMpw_-9cZfiYy8'};
 
 
 
 // RohS HesExaBot
-//module.exports = {telegraf_token: '426946045:AAE94D-fw19G9L7Bkpf0bCRh0Dkb9nKbqz0'};
+//module.exports = {API_KEY: '426946045:AAE94D-fw19G9L7Bkpf0bCRh0Dkb9nKbqz0'};
