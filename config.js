6a1114e81de8dad4a4651e5298102a10d08a19bd
R-Men <huser.gaetan@hotmail.com>
2018-06-12 12:06:47 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,1 @@
-
-
-// r-men bot api key
-module.exports = {API_KEY: '304562828:AAG7Vd1MaoMxvX8dMgZlNpMpw_-9cZfiYy8'};
-
-// hes-exa-bot api key
-//module.exports = {API_KEY: '426946045:AAE94D-fw19G9L7Bkpf0bCRh0Dkb9nKbqz0'};
+module.exports = {API_KEY: 'YOUR_API_KEY_HERE'};
